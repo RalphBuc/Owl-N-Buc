b045fac6c10a8f52fb73add6530f3765d6fa2177
Ralph <ralphmemee1234@gmail.com>
2022-01-12 23:48:43 +0800

    Third file updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './main.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import GalleryProvider from './contexts/providers/GalleryProvider';
 
 function App() {
     return (
         <div className='App'>
-            <GalleryProvider>
-                <HelmetProvider>
-                    <Home />
-                </HelmetProvider>
-            </GalleryProvider>
+            <Router>
+                <GalleryProvider>
+                    <HelmetProvider>
+                        <Switch>
+                            <Route path='/' exact component={Home} />
+                        </Switch>
+                    </HelmetProvider>
+                </GalleryProvider>
+            </Router>
         </div>
     );
 }
